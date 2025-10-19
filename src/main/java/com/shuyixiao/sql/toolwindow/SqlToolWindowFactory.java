5b8a5ef1b00bf4ad2fa6b9e8c24789be47b553b0
ShuYixiao <chinasjh2022@126.com>
2025-10-19 16:49:09 +0800

    :bug: 工具窗口管理器获取内容工厂添加sql窗口

--- src/main/java/com/shuyixiao/sql/toolwindow/SqlToolWindowFactory.java
+++ src/main/java/com/shuyixiao/sql/toolwindow/SqlToolWindowFactory.java
@@ -4,7 +4,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
-import com.intellij.ui.content.ContentFactory;
 import com.shuyixiao.sql.ui.SqlToolWindow;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,9 +15,12 @@ public class SqlToolWindowFactory implements ToolWindowFactory {
     
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        // 创建 SQL Monitor UI 组件
         SqlToolWindow sqlToolWindow = new SqlToolWindow(project);
-        ContentFactory contentFactory = ContentFactory.getInstance();
-        Content content = contentFactory.createContent(sqlToolWindow, "", false);
+        
+        // 创建内容并添加到工具窗口
+        Content content = toolWindow.getContentManager().getFactory()
+                .createContent(sqlToolWindow, "", false);
         toolWindow.getContentManager().addContent(content);
     }
     
